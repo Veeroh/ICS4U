de1aac0c2c2991fd52389bf9c3f120b677a3e03a
Alex <93527958+Veetwo1633@users.noreply.github.com>
2023-09-20 20:20:20 -0400

    Add files via upload

---
+++ RST/src/rst/Gunfight.java
@@ -0,0 +1,10 @@
+package rst;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.*;
+
+public class Gunfight{
+
+}
---
+++ RST/src/rst/Humans.java
@@ -0,0 +1,25 @@
+package rst;
+
+public class Humans {
+	int legs;
+	int arms;
+	int eyes;
+	int nose;
+	int mouth;
+	String name;
+	String sex;
+	String horseName;
+	String whiskeyPreference;
+
+	public Humans() {
+		legs = 2;
+		arms = 2;
+		eyes = 2;
+		nose = 1;
+		mouth = 1;
+	}
+
+	public String whatIsYourName() {
+		return name;
+	}
+}
